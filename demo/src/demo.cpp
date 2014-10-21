80fe143f2b3a85994f41750e1e18ebd2640f2a13
jindong.zjd <jindong.zjd@alibaba-inc.com>
2014-10-21 17:53:03 +0800

    update

---
+++ demo/src/demo.cpp
@@ -0,0 +1,19 @@
+//============================================================================
+// Name        : demo.cpp
+// Author      : jordan
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+int main() {
+	char *s = "xxxxxxx";
+	strcat(s,"ddddddddd");
+
+	return 0;
+}
---
+++ demo_c/src/myh.h
@@ -0,0 +1,13 @@
+/*
+ * myh.h
+ *
+ *  Created on: 2014年10月17日
+ *      Author: jindong.zjd
+ */
+
+
+
+#ifndef MYH_H_
+#define MYH_H_
+
+#endif /* MYH_H_ */
