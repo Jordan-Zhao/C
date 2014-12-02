01586817420e3798496097b36913914d563a61b9
jindongzhao <jindongzhao@126.com>
2014-12-02 15:08:49 +0800

    log4cplus源码，已整理成linux的eclipse C++工程。可运行

---
+++ log4cplus/demos/log4cplus_demo.cpp
@@ -0,0 +1,51 @@
+
+#include <iostream>
+
+#include "../src/log4cplus/configurator.h"
+#include "../src/log4cplus/loggingmacros.h"
+#include "../src/log4cplus/customappender.h"
+#include "../src/log4cplus/loglog.h"
+
+using namespace std;
+using namespace log4cplus;
+
+static Logger logFile1 = Logger::getInstance("logFile1");
+static Logger logFile2 = Logger::getInstance("logFile2");
+
+#ifdef _WIN32
+void __stdcall customFunc(const char* sz)
+{
+	OutputDebugString(sz);
+}
+#else //__linux__
+void customFunc(const char* sz)
+{
+	//
+}
+#endif
+
+
+int main()
+{
+	log4cplus::CustomAppender::setCustomFunc(customFunc);
+	string propUrl = "/home/zjd/dev/ide/eclipse-cpp/workspace/log4cplus/demos/urconfig.properties";
+	log4cplus::PropertyConfigurator::doConfigure(propUrl);
+	LogLog::getLogLog()->setInternalDebugging(true);
+
+	for (int i = 0; i < 3; i++)
+	{
+		LOG4CPLUS_DEBUG(logFile1,	"1 logFile1.\n");
+		LOG4CPLUS_INFO(logFile1,	"2 logFile1.\n");
+		LOG4CPLUS_ERROR(logFile1,	"4 logFile1.\n");
+		LOG4CPLUS_FATAL(logFile1,	"5 logFile1.\n");
+
+		LOG4CPLUS_DEBUG(logFile2,	"1 logFile2.\n");
+		LOG4CPLUS_INFO(logFile2,	"2 logFile2.\n");
+		LOG4CPLUS_ERROR(logFile2,	"4 logFile2.\n");
+		LOG4CPLUS_FATAL(logFile2,	"5 logFile2.\n");
+	}
+
+	return 0;
+}
+
+
